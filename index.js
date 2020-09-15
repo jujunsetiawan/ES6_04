04783ddcc952265cb11cd24b351a8b4b8d4d622e
Your Name <you@example.com>
2020-09-15 17:03:14 +0700

    upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const prabu = ['rizal','prabumulih','smknegeriprabumulih','prabujaya','sukajadi']
+const [nama, asal, ...rest] = prabu;
+console.log(nama);//rizal
+console.log(asal);//prabumulih
+console.log(rest);//'smknegeriprabumulih','prabujaya','sukajadi'
